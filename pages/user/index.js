b2db6d7745ea18159ca41f69d21cdf13a7b3bbd6
Vivek Singh <viveksingh27795@gmail.com>
2022-04-04 03:20:32 +0530

    Complete

--- pages/user/index.js
+++ pages/user/index.js
@@ -12,15 +12,16 @@ const UserIndex = () =>{
     return (
         <>
         <h1  className="pt-5 text-center text-success">User DashBoard</h1> 
-        <div className="text-center ">
-        
-        {/* <pre>Name : {user.user.name}</pre>
+       
+        {user !== null && 
+         <div className="text-center ">
+        <pre>Name : {user.user.name}</pre>
         <pre>Email : {user.user.email}</pre>
         <pre>Mobile : {user.user.mobile}</pre>
         <pre>Institute : {user.user.institute}</pre>
-        <pre>Branch : {user.user.branch}</pre> */}
+        <pre>Branch : {user.user.branch}</pre>
         </div>
-        
+             }
         
         
       {/* <pre>{JSON.stringify(user,null,4)}</pre> */}
